f6f6c5033f6473bf3f3989050a85c68f64d781ec
HArshi2611 <harshikarabadiya@gamil.com>
2024-01-11 14:32:03 +0530

    update

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-
-let compt;
 function generateRandom() {
+  let compt;
   let randomno = Math.random() * 3;
   if (randomno > 0 && randomno <= 1) {
-    compt = 'Bat';
+    return 'Bat';
   }
   else if (randomno > 1 && randomno <= 2) {
-    compt = 'Ball';
+    return 'Ball';
   }
   else {
-    compt = 'Stump';
+    return 'Stump';
   }
+
+}
+function getresult(userMove, comptMove) {
+  if (userMove === 'Bat') {
+    if (comptMove === 'Ball') {
+      return 'user won.';
+    } else if (comptMove === 'Bat') {
+      return 'it is a tie';
+    }
+    else {
+      return 'compt won';
+    }
+  } else if (userMove === 'Ball') {
+    if (comptMove === 'Ball') {
+      return 'it is a tie.';
+    } else if (comptMove === 'Bat') {
+      return 'compt won';
+    }
+    else {
+      return 'user won';
+    }
+  } else {
+    if (comptMove === 'Ball') {
+      return 'comt won.';
+    } else if (comptMove === 'Bat') {
+      return 'user won';
+    }
+    else {
+      return 'it is tie';
+    }
+  }
+
+}
+function showresult(userMove, comptMove, result) {
+  alert(`You have chosen ${userMove}.  compt choice is ${comptMove} and  ${result}`);
 }
